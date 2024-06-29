c61339aea8b2eabeb0fad344351eb1411c374529
ChrisWenzlick <cwenzlick01@gmail.com>
2024-06-29 14:36:38 -0500

    Make number buttons update display

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+let displayValue = "";
+
+const displayContainer = document.querySelector(".display");
 
 
 const operate = function(operator, left, right) {
@@ -32,4 +35,28 @@ const multiply = function(left, right) {
 
 const divide = function(left, right) {
     return left / right;
+}
+
+const updateDisplay = function(newInput) {
+    displayValue += newInput;
+    displayContainer.textContent = displayValue;
+}
+
+const onButtonPressed = function(value) {
+    switch(value) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            updateDisplay(value);
+            break;
+        default:
+            break;
+    }
 }
